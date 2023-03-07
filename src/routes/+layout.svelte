b30394d7aacbefb0e9b9eb29ca29abcaca8a7fa5
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-03-07 10:39:49 +0100

    add Lenis scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,31 @@
 <script>
+  import Lenis from "@studio-freight/lenis";
+  import { onMount } from "svelte";
+
   import "../lib/styles/global.css";
   import NavHeader from "../components/NavHeader.svelte";
   import Footer from "../components/Footer.svelte";
+
+  onMount(() => {
+    const lenis = new Lenis({
+      duration: 1,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      direction: "vertical", // vertical, horizontal
+      gestureDirection: "vertical", // vertical, horizontal, both
+      smooth: true,
+      mouseMultiplier: 1,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
 </script>
 
 <NavHeader />
