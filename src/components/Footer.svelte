52d9bc1fc79cb7c707dc208322c4ae02fc3a2aa9
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-03-12 15:34:59 +0100

    add about page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
   <ul>
-    <li><a href="/">About</a></li>
+    <li><a href="/about">About</a></li>
     <li><a href="/contact">Contact</a></li>
   </ul>
 </footer>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,63 @@
+<section class="about-page">
+  <h1 class="about-page-header">Rainy Days</h1>
+  <h2 class="about-page-slogan">Pushing the comfort zone</h2>
+  <div class="text">
+    <p>
+      Rainy Days is an online shop that specializes in selling high-quality rain
+      jackets for men and women. Our primary focus is on designing jackets that
+      cater to a range of different outdoor activities that enrich people's
+      lives. Rainy Days is dedicated to providing comfortable, durable, and
+      affordable jackets that help people push their comfort zones and explore
+      the great outdoors without worrying about the weather.
+    </p>
+    <p>
+      At Rainy Days, we understand that being outdoors is a way of life for many
+      people, and we aim to provide them with the right gear that can withstand
+      any weather condition.
+    </p>
+    <p>
+      We take pride in our unique selling points. Our jackets are mid-range in
+      price, which means that they are affordable without compromising on
+      quality. Our focus is on durability and versatility, ensuring that our
+      customers are comfortable whatever adventure they go on. Our jackets are
+      suitable for a range of different weather types, making them ideal for
+      people who are always on the move and exploring new destinations. Our
+      slogan "Pushing the Comfort Zone" reflects our commitment to providing our
+      customers with the right gear to help them go beyond their limits and push
+      their boundaries.
+    </p>
+    <p>
+      At Rainy Days, we believe that everyone should have the freedom to explore
+      the great outdoors, no matter what the weather is like.
+    </p>
+  </div>
+
+  <div class="images">
+    <img src="/assets/images/hero/hero-29.webp" alt="" />
+  </div>
+</section>
+
+<style>
+  .about-page {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .text {
+    max-width: 70ch;
+  }
+
+  h1 {
+    font-size: 5rem;
+    text-transform: uppercase;
+  }
+
+  h2 {
+    margin-block: 1rem;
+  }
+
+  p {
+    margin-block-end: 1rem;
+  }
+</style>
