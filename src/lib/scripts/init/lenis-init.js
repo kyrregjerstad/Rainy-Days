9df14ad242b955b5e5b1d91ea665fbab0a8ea23f
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-04-14 12:40:01 +0200

    style reduce scroll smoothing

--- src/lib/scripts/init/lenis-init.js
+++ src/lib/scripts/init/lenis-init.js
@@ -2,7 +2,7 @@ import Lenis from "@studio-freight/lenis";
 
 export function lenisInit() {
   const lenis = new Lenis({
-    duration: 1.1,
+    duration: 0.9,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     direction: "vertical",
     gestureDirection: "vertical",
