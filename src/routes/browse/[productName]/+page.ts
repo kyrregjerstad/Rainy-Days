10d558089ad2f34c106acd738cc73fd417066a63
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-24 11:59:19 +0200

    remove console .log

--- src/routes/browse/[productName]/+page.ts
+++ src/routes/browse/[productName]/+page.ts
@@ -1,8 +1,5 @@
 export const load = async ({ url }) => {
-  // should return the slug in the url after ?id=
   const slugId = url.searchParams.get("id");
-
-  console.log(url.searchParams.get("id"));
 
   return {
     slugId,
