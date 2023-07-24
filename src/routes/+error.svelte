bd3b88519943cba9926b5bf173a0a81a2f2af871
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-07-24 11:07:31 +0200

    add error maintenance page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<div>
+  <h1>This page is temporary down for maintenance</h1>
+  <h2>Please come back soon!</h2>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    text-align: center;
+  }
+</style>
